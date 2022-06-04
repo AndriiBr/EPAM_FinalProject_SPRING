e12f84acc5ef9208569494fdd6387d72d45cdda0
Andrii Brazhnyk <kamineko911@gmail.com>
2022-06-04 13:52:54 +0300

    Initial commit

---
+++ src/main/java/com/brazhnyk/epam_finalproject_spring/EpamFinalProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.brazhnyk.epam_finalproject_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamFinalProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpamFinalProjectSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brazhnyk/epam_finalproject_spring/EpamFinalProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazhnyk.epam_finalproject_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpamFinalProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
